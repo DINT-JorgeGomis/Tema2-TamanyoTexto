4493b05d3edb10406e96e15f3749512bd657e1e1
Jorge Gomis Román <jorge.gomis@iesdoctorbalmis.com>
2023-10-03 10:01:30 +0200

    Terminado

---
+++ Tema2-TamanyoTexto/Tema2-TamanyoTexto/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_TamanyoTexto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textoRadioButtonChecked(object sender, RoutedEventArgs e) => textoLabel.FontSize = (sender as RadioButton).Tag switch
+        {
+            "pequeño" => 15,
+            "mediano" => 20,
+            "grande" => 25
+        };
+    }
+}
